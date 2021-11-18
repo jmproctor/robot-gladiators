6f08b0352de6cfcd7df554c6ff1806d66635ba3b
JMP <jmproctor14@live.com>
2021-11-17 19:15:57 -0600

    added conditions to .js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,45 @@
-function fight() {
-    window.alert("The fight has begun!");
-}
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("This logs a string, good for leaving yourself a message");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(10 + 10);
+var fight = function() {
+    //alert players that the round is starting
+  window.alert("Welcome to Robot Gladiators!");
 
-console.log("Our robot's name is " + playerName);
+  //subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
 
-var name = "your name";
-console.log(name);
+  //Log a resulting message to the console so we know that it worked
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
 
-// fight();
+    if (enemyHealth <= 0) {
+      window.alert(enemyName + " has died!");
+    }
+    else {
+      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+  //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
+    playerHealth = playerHealth - enemyAttack;
+
+  //Log a resulting message to the console so we know that it worked
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+    if(playerHealth <= 0) {
+      window.alert(playerName + " has died!");
+    }
+
+    else {
+      window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+};
+
+fight();
